f81c73cc6aba53597866eed3e6bcfcb1fd075734
steven.nevins.js@gmail.com <steven.nevins.js@gmail.com>
2020-11-02 10:44:27 -0500

    updated tokenfactory

--- packages/buidler/contracts/HPoolFactory.sol
+++ packages/buidler/contracts/HPoolFactory.sol
@@ -1,10 +1,13 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./base/HPool.sol";
 
 contract HPoolFactory{
-        address public fee;
-        constructor() public {
-                fee = 0x8E0647590c0Bb51a47d43280D774818a5c053e5c;
+        event PoolMinted(address tokenAddress);
+
+        function deployNewPool() 
+                public returns (address){
+                HPool p = new HPool();
+                emit PoolMinted(address(p));
         }
 }
