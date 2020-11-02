fcd532f6a9f64d7a56a64c79aca8223d27a0ee3f
steven.nevins.js@gmail.com <steven.nevins.js@gmail.com>
2020-11-02 17:07:47 -0500

    setting up factories

--- packages/buidler/contracts/HDealerFactory.sol
+++ packages/buidler/contracts/HDealerFactory.sol
@@ -5,11 +5,12 @@ import "./base/HDealer.sol";
 
 contract HDealerFactory{
         event DealerMinted(address tokenAddress);
+        address private _poolFactory;
 
         mapping(address=>bool) public dealers;
         function deployNewDealer() 
                 public returns (address){
-                HDealer d = new HDealer();
+                HDealer d = new HDealer(_poolFactory);
                 dealers[address(d)]=true;
                 emit DealerMinted(address(d));
         }
--- packages/buidler/contracts/HPoolFactory.sol
+++ packages/buidler/contracts/HPoolFactory.sol
@@ -5,11 +5,12 @@ import "./base/HPool.sol";
 contract HPoolFactory{
         mapping(address=>address) public getPool;
         event PoolMinted(address tokenAddress);
+        address private _dealerFactory;
 
         function deployNewPool(address token) 
                 public returns (address){
                         require(getPool[token] == address(0), "Pool exists");
-                        HPool p = new HPool(token);
+                        HPool p = new HPool(token, _dealerFactory);
                         getPool[token]=address(p);
                         emit PoolMinted(address(p));
         }
