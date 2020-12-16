e9258d9ba1d16408c2dbecf357dc09bd093371af
steven <steven.nevins.js@gmail.com>
2020-12-16 12:25:51 -0500

    updated infura key

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "5ce0898319eb4f5c9d4c982c8f78392a";
+export const INFURA_ID = "6ed831aea5e4492097496271e02a95f0";
 
 //MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
